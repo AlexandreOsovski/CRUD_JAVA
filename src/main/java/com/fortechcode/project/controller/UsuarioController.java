792a117aa7f85b050a35cb1a67294cbf20d265e5
Alexandre Valente Osovski <alexandre.osovski@tecpuc.com.br>
2023-05-18 22:25:11 -0300

    alterar informações do usuario

--- src/main/java/com/fortechcode/project/controller/UsuarioController.java
+++ src/main/java/com/fortechcode/project/controller/UsuarioController.java
@@ -10,8 +10,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import com.fortechcode.project.model.UsuarioModel;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class UsuarioController {
@@ -24,26 +25,18 @@ public class UsuarioController {
         this.usuarioRepository = usuarioRepository;
     }
 
-
-
-    private ModelAndView home(){
-        ModelAndView modelAndView = new ModelAndView("Dashboard/dashboard");
-        return modelAndView;
-    }
-
-    @RequestMapping(value = "/usuarioLogin/", method = RequestMethod.POST)
-    public String loginUsuario(@Valid @ModelAttribute UsuarioModel usuarioModel, BindingResult result) {
+    @PostMapping("/usuarioLogin/")
+    public String loginUsuario(@Valid @ModelAttribute UsuarioModel usuarioModel, BindingResult result, Model model) {
         UsuarioModel verificaUsuario = usuarioRepository.findByEmail(usuarioModel.getEmail());
-        RedirectView redirectView = new RedirectView();
+
         if (verificaUsuario == null || !verificaUsuario.getSenha().equals(usuarioModel.getSenha())) {
-            result.rejectValue("email", "email or password incorrect");
-//            ModelAndView modelAndView = new ModelAndView("Usuario/login");
-//            return modelAndView;
+            result.rejectValue("email", "incorrectCredentials", "Email or password incorrect");
+            return null;
+        } else {
+            model.addAttribute("usuario", verificaUsuario);
+            return "Dashboard/dashboard";
         }
-
-        return "redirect:/Dashboard/dashboard";
     }
-
 
     @RequestMapping(value = {"/getUsuarios/{id}"}, method = RequestMethod.GET)
     public ResponseEntity findById(@PathVariable long id) {
@@ -85,37 +78,43 @@ public class UsuarioController {
     }
 
 
-    @PutMapping("/alterarUsuario/{id}")
-    public ResponseEntity<UsuarioModel> updateUsuario(@PathVariable long id, @RequestBody UsuarioModel usuarioAtualizado) {
+    @PostMapping("/alterarUsuario/")
+    public ResponseEntity<UsuarioModel> updateUsuario(@Valid @ModelAttribute     UsuarioModel usuarioAtualizado) {
+
+        long id = usuarioAtualizado.getId();
 
         UsuarioModel usuarioExistente = usuarioRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado com id: " + id));
 
-        if (usuarioAtualizado.getNome() != null) {
-            usuarioExistente.setNome(usuarioAtualizado.getNome());
-        }
-        if (usuarioAtualizado.getSobrenome() != null) {
-            usuarioExistente.setSobrenome(usuarioAtualizado.getSobrenome());
-        }
-        if (usuarioAtualizado.getEmail() != null) {
-            UsuarioModel verificaEmail = usuarioRepository.findByEmail(usuarioAtualizado.getEmail());
-            if (verificaEmail != null && verificaEmail.getId() != usuarioExistente.getId()) {
-                throw new IllegalArgumentException("Já existe esse email cadastrado em nosso banco de dados: " + usuarioAtualizado.getEmail());
+        if(id == usuarioExistente.getId()){
+            if (usuarioAtualizado.getNome() != null) {
+                usuarioExistente.setNome(usuarioAtualizado.getNome());
             }
-            usuarioExistente.setEmail(usuarioAtualizado.getEmail());
+            if (usuarioAtualizado.getSobrenome() != null) {
+                usuarioExistente.setSobrenome(usuarioAtualizado.getSobrenome());
+            }
+            if (usuarioAtualizado.getEmail() != null) {
+                UsuarioModel verificaEmail = usuarioRepository.findByEmail(usuarioAtualizado.getEmail());
+                if (verificaEmail != null && verificaEmail.getId() != usuarioExistente.getId()) {
+                    throw new IllegalArgumentException("Já existe esse email cadastrado em nosso banco de dados: " + usuarioAtualizado.getEmail());
+                }
+                usuarioExistente.setEmail(usuarioAtualizado.getEmail());
+            }
+            if (usuarioAtualizado.getSenha() != null) {
+                usuarioExistente.setSenha(usuarioAtualizado.getSenha());
+            }
         }
-        if (usuarioAtualizado.getSenha() != null) {
-            usuarioExistente.setSenha(usuarioAtualizado.getSenha());
-        }
+
 
         UsuarioModel usuarioAtualizadoSalvo = usuarioRepository.save(usuarioExistente);
         return ResponseEntity.ok(usuarioAtualizadoSalvo);
+
     }
 
 
 
     @DeleteMapping(path ={"/deleteUsuario/{id}"})
-    public ResponseEntity <?> deleteUsuario(@PathVariable long id) {
+    public ResponseEntity <?> deleteUsuario(@Valid @ModelAttribute long id) {
 
         return usuarioRepository.findById(id)
                 .map(record -> {
